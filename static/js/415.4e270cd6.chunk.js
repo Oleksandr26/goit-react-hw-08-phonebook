7e1acab49ee432e4aa0b42fdb967537acc629f8b
Oleksandr26 <103263665+Oleksandr26@users.noreply.github.com>
2022-09-08 10:03:25 +0000

    Deploying to gh-pages from @ Oleksandr26/goit-react-hw-08-phonebook@eda1914bdefb327384019a2af6e015d15b9b4aa5 🚀

---
+++ static/js/415.4e270cd6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(e,t,o){o.r(t);var n=o(184);t.default=function(){return(0,n.jsx)("h1",{style:{textAlign:"center",marginBottom:"30px"},children:"Welcome to the Phonebook"})}}}]);
+//# sourceMappingURL=415.4e270cd6.chunk.js.map
